89c3a03b7a769e5adb5a4f44300fa52420647437
Joshua Howard <joshthoward@gmail.com>
2017-06-05 16:41:59 -0400

    Testing template added

---
+++ test/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class basic_test_case(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def basic_test(self):
+        pass
+
+if __name__ == '__main__': unittest.main()
